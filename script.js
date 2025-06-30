756a57f23b3c825673276299577f749cefd375c1
emmanuelybrandao <hyuugapft@gmail.com>
2025-06-29 21:28:21 -0300

    update js file

--- script.js
+++ script.js
@@ -24,18 +24,33 @@ function jogoNumeroSecreto() {
 }
 
 function jogoMedia() {
-    alert('Seja-bem vindo ao cálculo de notas.');
+    alert("Bem-vindo ao Jogo da Média!");
 
-    let notas = prompt("Digite suas notas separadas por vírgula:");
-    let vetorNotas = notas.split(",");
+    let quantidade = prompt("Quantos números você quer digitar?");
+    quantidade = Number(quantidade);
 
-    let somaNotas = 0;
-    for (let i = 0; i < vetorNotas.length; i++) {
-    somaNotas += Number(vetorNotas[i]);
+    if (quantidade <= 0 || quantidade === null || quantidade === "" || typeof quantidade !== "number") {
+    alert("Quantidade inválida. Tente novamente com um número maior que zero.");
+    } else {
+    let soma = 0;
+
+    for (let i = 1; i <= quantidade; i++) {
+        let entrada = prompt(`Digite o ${i}º número:`);
+        let numero = Number(entrada);
+
+        if (entrada.trim() === "" || entrada === null || isNaN(numero)) {
+        alert("Valor inválido. Digite um número.");
+        i--; // repetir a rodada
+        continue;
+        }
+
+        soma += numero;
     }
 
-    let media = somaNotas / vetorNotas.length;
-    alert(`A sua média final é ${media}.`);
+    let media = soma / quantidade;
+
+    alert(`A média calculada entre os ${quantidade} números digitados é: ${media.toFixed(2)}`);
+    } 
 }
 
 function somaNumeros() {
